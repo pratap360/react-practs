c3eef1440d2e9514d2066e7bf6eba6a500722e7b
pratap360 <pratapparui0360@gmail.com>
2023-06-20 12:43:59 +0530

    tut 10 outputting list

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,23 @@
 import { useState } from "react";
 
 const Home = () => {
+    const [blogs,setBlogs] = useState([
+        {title:'mere blog hai',body:'lorem ipsum...', author:'bapi',id:1},
+        {title:'ayush ka blog hai',body:'lorem ipsum...', author:'arusho',id:2},
+        {title:'tera blog hai',body:'lorem ipsum...', author:'raja',id:3}
+    ]);
 
-    // let name = 'bapi' ;
-    const [name,setName] = useState('Ayush');
-    const[age,setAge] = useState(12);
-
-const handleClick = () => {
-    // using use state hooks 
-    setName('Pratap');
-    setAge(21);
-//    name = 'pratap'
-//    console.log(name);
- 
-}
-    return ( 
-        <div className="homepage">
-            <h2>Homepage</h2>
-            <p>Hey I'm {name} & My age is {age} years old</p>
-            <button onClick={handleClick} >click me</button>
+    return(
+        <div className="home">
+            {blogs.map((blog) =>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                </div>
+            ))}
         </div>
-     );
+    );
 }
+   
  
 export default Home;
-
-// tutorials pratice of click events 
-
-// const Home = () => {
-
-//     const handleClick = (e) => {
-//         console.log('hello, parui',e);
-//     }
-    
-//     const handleClickAgain = (name,e) => {
-//         console.log('hello'+ name,e.target);
-//     }
-    
-//         return ( 
-//             <div className="homepage">
-//                 <h2>Homepage</h2>
-//                 <button onClick={handleClick} >click me</button>
-//                 <button onClick={(e) => {handleClickAgain('bapi',e)}}>click me again</button>
-//             </div>
-//          );
-//     }
-     
---
+++ tuts 7-8.md
@@ -0,0 +1,54 @@
+// tutorials pratice of click events 
+
+// const Home = () => {
+
+//     const handleClick = (e) => {
+//         console.log('hello, parui',e);
+//     }
+    
+//     const handleClickAgain = (name,e) => {
+//         console.log('hello'+ name,e.target);
+//     }
+    
+//         return ( 
+//             <div className="homepage">
+//                 <h2>Homepage</h2>
+//                 <button onClick={handleClick} >click me</button>
+//                 <button onClick={(e) => {handleClickAgain('bapi',e)}}>click me again</button>
+//             </div>
+//          );
+//     }
+
+
+<!-- tutorils 8  -->
+
+import { useState } from "react";
+
+const Home = () => {
+
+    // let name = 'bapi' ;
+    const [name,setName] = useState('Ayush');
+    const[age,setAge] = useState(12);
+
+const handleClick = () => {
+    // using use state hooks 
+    setName('Pratap');
+    setAge(21);
+//    name = 'pratap'
+//    console.log(name);
+ 
+}
+    return ( 
+        <div className="homepage">
+            <h2>Homepage</h2>
+            <p>Hey I'm {name} & My age is {age} years old</p>
+            <button onClick={handleClick} >click me</button>
+        </div>
+     );
+}
+ 
+export default Home;
+
+
+     
+     
