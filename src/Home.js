d496359b5dd01d416f25e2d74d68fa3c4a49dadf
pratap360 <pratapparui0360@gmail.com>
2023-06-21 13:13:17 +0530

    tutorials 19 & 20

--- src/Home.js
+++ src/Home.js
@@ -1,27 +1,16 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setBlogs (data);
-            setIsPending (false);
-        })
-        },1000)
-    }, [])
+    const { error, isPending, data : blogs} = useFetch('http://localhost:8000/blogs')
+    // const {data:blogs,error,isPending} = useFetch('http://localhost:8000/blogs')
 
     return (
         <div className="home">
             {isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs !" />}
+            {error && <div>{ error }</div> }
+            {blogs && <BlogList blogs={blogs} title="All Blogs !"/>}
             {/* <BlogList blogs={blogs.filter((blog)=>blog.author==='raja')} title="Raja's Blogs !"/> */}
             {/* <button onClick={() => setName('raja')}>change name</button> */}
             {/* <p>{name}</p> */}
