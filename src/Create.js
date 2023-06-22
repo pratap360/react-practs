d46b5e94e3ab10323e503fed477adaa3a7cc581a
pratap360 <pratapparui0360@gmail.com>
2023-06-22 16:42:07 +0530

    adding the blog delete

--- src/Create.js
+++ src/Create.js
@@ -1,50 +1,53 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
-
-    const [title,setTitle] = useState('');
-    const [body,setBody] = useState('');
-    const [author,setAuthor] = useState('bapi');
-    const [isPending,setIsPending] = useState(false);
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false)
+    const history = useHistory();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const blog = {title,body,author}
+        const blog = { title, body, author };
 
         // console.log(blog);
         setIsPending(true);
 
-        fetch('http://localhost:8000/blogs',{
+        fetch('http://localhost:8000/blogs/', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(blog)
-        }).then(()=> {
+        }).then(() => {
             console.log('new blog added');
             setIsPending(false)
+            // history.go(-1);
+            history.push('/');
         })
-    } 
+    }
 
-    return ( 
+    return (
         <div className="create">
-            <h2>Add your blogs</h2>
-            <form onClick={handleSubmit}>
-                <label>Blog Title:</label>
-                <input 
-                type="text" 
-                required
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
+            <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
-                <label>Blog Body:</label>
-                <textarea 
-                value={body}
-                onChange = {(e) => setBody(e.target.value)}
-                required>
-                </textarea>
-                <label>Blog Author:</label>
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
                 <select
-                value={author}
-                onChange= {(e) => setAuthor(e.target.value)}
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
                 >
                     <option value="bapi">bapi</option>
                     <option value="arusho">arusho</option>
@@ -57,7 +60,7 @@ const Create = () => {
                 <p>{author}</p> */}
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
